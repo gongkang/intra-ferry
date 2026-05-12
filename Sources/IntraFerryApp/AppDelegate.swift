711805b9030658888a09fbebb865d09fa6f74d0c
kuchen <kuchen@mengxiang.com>
2026-05-12 18:12:11 +0800

    feat: polish transfer window footer

--- Sources/IntraFerryApp/AppDelegate.swift
+++ Sources/IntraFerryApp/AppDelegate.swift
@@ -71,7 +71,7 @@ final class AppDelegate: NSObject, NSApplicationDelegate, NSWindowDelegate {
         }
 
         let window = NSWindow(
-            contentRect: NSRect(x: 0, y: 0, width: 680, height: 540),
+            contentRect: NSRect(x: 0, y: 0, width: 820, height: 560),
             styleMask: [.titled, .closable, .resizable],
             backing: .buffered,
             defer: false
